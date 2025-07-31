3bfe04bb82f12900d8df268ca6e9df5f786f40e5
ZXerniXZ <ZXerniXZ@gmail.com>
2025-07-31 11:39:05 +0200

    l

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Funzione per richiedere il permesso delle notifiche
+export async function richiediPermessoNotifiche() {
+  if (!('Notification' in window)) {
+    console.log('Questo browser non supporta le notifiche desktop');
+    return 'denied';
+  }
+
+  if (Notification.permission === 'granted') {
+    return 'granted';
+  }
+
+  if (Notification.permission === 'denied') {
+    return 'denied';
+  }
+
+  const permission = await Notification.requestPermission();
+  return permission;
+}
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(); 
